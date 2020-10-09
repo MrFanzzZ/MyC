db81fdc5bc53baa480ed981d5e245c523d3a63e2
fan <mrfanzzz@qq.com>
2020-10-09 21:07:33 +0800

    a

---
+++ manager/boss.h
@@ -0,0 +1,16 @@
+
+#pragma once
+#include"worker.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class Boss :public Worker
+{
+public:
+	Boss(int id, string name, int did);
+	virtual void showInfo();
+	virtual string getDeptname();
+
+
+
+};
